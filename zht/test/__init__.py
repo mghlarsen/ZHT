5cdcc1db8fc14e179f8f6fe7e7ba97dc332faeb4
Michael Larsen <mike.gh.larsen@gmail.com>
2011-10-20 23:48:30 -0600

    Added tests for synchronization, tests now yield to other greenlets where needed.

--- zht/test/__init__.py
+++ zht/test/__init__.py
@@ -1,4 +1,5 @@
 import zmq
+import gevent
 from zht.shell import ZHTControl
 from zht.node import Node
 from unittest import TestCase
@@ -15,18 +16,41 @@ def closeNode(node, control):
     control.EOF()
     node._greenletPool.join()
 
+def clearWaitingGreenlets(n=10):
+    """
+    Yield enough to clear any greenlets waiting on I/O, etc.
+    If there's a test failure, it may be necessary to up n for that attempt.
+    """
+    for i in range(2**n):
+        gevent.sleep(0)
+
 class TestZHT(TestCase):
     def setUp(self):
         self.aNode, self.aControl = initNode('a', None)
         self.bNode, self.bControl = initNode('b', None)
 
     def tearDown(self):
+        closeNode(self.aNode, self.aControl)
+        closeNode(self.bNode, self.bControl)
         self.aNode = self.aControl = self.bNode = self.bControl = None
 
     def testGet(self):
+        self.assertEqual(self.aControl.get(['asdf']), ['KeyError'])
+        self.assertEqual(self.bControl.get(['asdf']), ['KeyError'])
+
+    def testPut(self):
         self.assertEqual(self.aControl.get(['asdf']), ['KeyError'])
         self.assertEqual(self.aControl.put('asdf', 'qwer'), ['OK', 'asdf', 'qwer'])
         self.assertEqual(self.aControl.get(['asdf']), ['qwer'])
         self.assertEqual(self.bControl.get(['asdf']), ['KeyError'])
 
-
+    def testSync(self):
+        self.assertEqual(self.aControl.get(['asdf']), ['KeyError'])
+        self.assertEqual(self.aControl.put('asdf', 'qwer'), ['OK', 'asdf', 'qwer'])
+        self.assertEqual(self.aControl.get(['asdf']), ['qwer'])
+        self.assertEqual(self.bControl.get(['asdf']), ['KeyError'])
+        self.assertEqual(self.bControl.connect(['ipc://testSockaREP']), ['OK'])
+        clearWaitingGreenlets(12)
+        self.assertEqual(self.aControl.get(['asdf']), ['qwer'])
+        self.assertEqual(self.bControl.get(['asdf']), ['qwer'])
+        
