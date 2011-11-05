aea308777c939cb03e258e134370b8e29fc87ded
Michael Larsen <mike.gh.larsen@gmail.com>
2011-11-05 11:43:23 -0600

    Add documentation to zht.version

--- zht/version.py
+++ zht/version.py
@@ -1,3 +1,10 @@
+"""
+This module holds the version information for ZHT. The idea is that this can be safely imported from anywhere
+without worrying that something would break.
+
+:data:`packageVersion`
+    This is the current package version, as a string.
+"""
 
 packageVersion = '0.0.1a1'
 
